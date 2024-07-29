0de295c5577db1b669b07476850d6982e32657f1
bangdv <123456a@>
2024-07-29 10:25:42 +0700

    first commit

---
+++ src/src/Animal.java
@@ -0,0 +1,6 @@
+public abstract class Animal {
+    public abstract void ank();
+    public void keu(){
+        System.out.println("Keu");
+    }
+}
---
+++ src/src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog extends Animal{
+    @Override
+    public void ank(){
+        System.out.println("xương");
+    }
+    public void keu(String keu){
+        System.out.println("Chó sủa: " + keu);
+    }
+}
